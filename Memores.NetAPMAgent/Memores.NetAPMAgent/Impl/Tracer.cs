f25003b99f2060e2b8e990cd72920d0bf46d9828
memores <pdorovsky@gmail.com>
2018-07-31 17:53:13 +0300

    Object pool implementation

---
+++ Memores.NetAPMAgent/Memores.NetAPMAgent/Impl/Tracer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Memores.NetAPMAgent.Model;
+
+
+namespace Memores.NetAPMAgent.Impl
+{
+    public class Tracer: ITracer
+    {
+        public Transaction StartTransaction() {
+            throw new NotImplementedException();
+        }
+
+
+        public Span StartSpan() {
+            throw new NotImplementedException();
+        }
+
+
+        public void CaptureException(Exception e) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Memores.NetAPMAgent/Memores.NetAPMAgent/ObjectPool.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Memores.NetAPMAgent {
+    /// <summary>
+    /// Object pool can improve application performance in situations where you require multiple instances of a class and the class is expensive to create or destroy
+    /// </summary>
+    /// <see>
+    ///     <cref>https://docs.microsoft.com/ru-ru/dotnet/standard/collections/thread-safe/how-to-create-an-object-pool</cref>
+    /// </see>
+    /// <typeparam name="T"></typeparam>
+    public class ObjectPool<T> {
+        ConcurrentBag<T> _objects;
+        Func<T> _objectGenerator;
+
+
+        public ObjectPool(Func<T> objectGenerator) {
+            _objects = new ConcurrentBag<T>();
+            _objectGenerator = objectGenerator ?? throw new ArgumentNullException(nameof(objectGenerator));
+        }
+
+
+        public T GetObject() {
+            T item;
+            if (_objects.TryTake(out item))
+                return item;
+            return _objectGenerator();
+        }
+
+
+        public void PutObject(T item) {
+            _objects.Add(item);
+        }
+    }
+}
